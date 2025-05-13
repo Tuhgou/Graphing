159b2c92fc20585f07fa1ae66edab3d39156b4eb
Tuhgou <Jacquesjacq0644@gmail.com>
2025-05-13 11:50:41 +0200

    m

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const x = [];
+  
+    x[0] = x0;
+    x[1] = x1;
+    x[2] = x2;
+    x[3] = x3;
+    x[4] = x4;
+    x[5] = x5;
+    x[6] = x6;
+    x[7] = x7;
+    x[8] = x8;
+    x[9] = x9;
+    x[10] = x10;
+    x[11] = x11;
+    x[12] = x12;
+    x[13] = x13;
+    x[14] = x14;
+    x[15] = x15;
+    x[16] = x16;
+    x[17] = x17;
+    x[18] = x18;
+    x[19] = x19;
+    x[20] = x20;
+
+function myFunction() {
+    switch (x) {
+        case 1:
+                //ax+b//
+
+            document.getElementById("demo").innerHTML = x[0];
+            break;
+        case 2:
+                //ax^2+bx+c//
+
+            document.getElementById("demo").innerHTML = x[1];
+            break;
+        case 3:
+                //ax^3+bx^2+cx+d//
+
+            document.getElementById("demo").innerHTML = x[2];
+            break;
+    }
+}
