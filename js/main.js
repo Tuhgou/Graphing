290b02d65201cc03d02ea9727bb3722b76c4e8de
Tuhgou <Jacquesjacq0644@gmail.com>
2025-05-15 00:07:45 +0200

    m

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { beregn } from "../modules/beregning.js";
 
 //document.getElementById("menu").innerHTML;
 
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 21; i++) {
     beregn();
     
 }
--- modules/beregning.js
+++ modules/beregning.js
@@ -2,26 +2,30 @@ var x = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10];
 let y = 0;
 
 var i = 0;
-let f = 1;
+let f = 3;
 
-let a = 2;
-let b = 6;
-let c = 4;
+let a = 1.2;
+let b = 2;
+let c = 0;
 let d = 0;
 
     function beregn() {
         if (f==0){
             y=a*x[i]+b;
             console.log(y);
+            i++;
         } else if (f==1) {
-            y=a*x[i]^2+b*x[i]+c;  
+            y=a*x[i]*x[i]+b*x[i]+c;  
             console.log(y); 
+            i++;
         }else if (f==2){
-            y=a*x[i]^3+b*x[i]^2+c*x[i]+d;
+            y=a*x[i]*x[i]*x[i]+b*x[i]*x[i]+c*x[i]+d;
             console.log(y);
+            i++;
         } else if (f==3){
-            y=b+x[i]^a;
+            y=b*(x[i])^a;
             console.log(y);
+            i++;
         }
 }
 
