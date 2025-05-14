ece6dca07323193e2734a05ef8754ed49dfc3a62
Tuhgou <Jacquesjacq0644@gmail.com>
2025-05-14 18:11:46 +0200

    m

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
+import { beregn } from "../modules/beregning.js";
 
-import { beregn } from "./js/main copy.js";
-
-document.getElementById("menu").innerHTML;
+//document.getElementById("menu").innerHTML;
 
 for (var i = 0; i < 20; i++) {
     beregn();
