d018327e9ef78abc5142fd2d8a6fca9c3d447253
Tuhgou <Jacquesjacq0644@gmail.com>
2025-05-14 07:59:23 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-int x[] = {
-    -10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10
-}
+var x = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10];
 
-document.getElementById("demo").innerHTML
+var i = 0;
 
-function myFunction() {
-    if (){
+document.getElementById("menu").innerHTML;
 
-    } elseif (){
-
-    } elseif (){
-
-    } elseif (){
-
-    };
-
+for (var i = 0; i < 20; i++) {
+    function myFunction() {
+        if (f=0){
+            a*x[i]+b;
+        } elseif (f=1);{
+            a*x[i]^2+b*x[i]+c;  
+        } elseif (f=2);{
+            a*x[i]^3+b*x[i]^2+c*x[i]+d;
+        } elseif (f=3);{
+            b+x[i]^a;
+        }
+    }
 }
